77e1acbde8296e183ca5556a21a7f935c2bf2c0f
hjson9181 <haejinson0704@gmail.com>
2026-05-24 14:20:59 +0000

    saving our two init files

---
+++ scripts/init_git_creds.sh
@@ -0,0 +1,11 @@
+!#/usr/bin/bash
+
+USER=haejinson0704@gmail.com
+NAME=hjson9181
+
+git config --global --list
+
+git config --global user.email ${USER} 
+git config --global user.name  ${NAME} 
+
+git config --global --list
